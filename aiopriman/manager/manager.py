0d3bafcd552972fd2a713fe2aaad30a0d7f5d9f2
darksidecat <bitalik371@gmail.com>
2021-05-13 12:04:16 +0300

    remove cast

--- aiopriman/manager/manager.py
+++ aiopriman/manager/manager.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 import inspect
 from functools import partial
-from typing import List, Optional, Type, TypeVar, Union, cast
+from typing import List, Optional, Type, TypeVar, Union
 
 from aiopriman.storage import BaseStorage, StorageData
 from aiopriman.sync_primitives import SyncPrimitive
@@ -40,9 +40,11 @@ class Manager:
         T_Storage : subclass of SyncPrimitiveStorage
     """
 
-    def __init__(self,
-                 types: Type[Types] = Types,
-                 storage_data: Optional[StorageData[T_co]] = None):
+    def __init__(
+            self,
+            types: Type[Types] = Types,
+            storage_data: Optional[StorageData[T_co]] = None
+    ):
         """
         :param types: manager types
         :param storage_data: StorageData
@@ -50,10 +52,13 @@ class Manager:
         self.types = types
         self.storage_data = storage_data if storage_data else StorageData()
 
-    def get(self,
+    def get(
+            self,
             man_type: Union[
                 Type[BaseManager[T_co, T_Storage]],
-                str]) -> Type[BaseManager[T_co, T_Storage]]:
+                str
+            ]
+    ) -> partial[BaseManager[T_co, T_Storage]]:
         """
         Get class based on man_type value with settled with functools.partial storage_date
 
@@ -61,15 +66,10 @@ class Manager:
         """
         if isinstance(man_type, str):
             if man_type in self.types.props():
-                return cast(
-                    Type[BaseManager[T_co, T_Storage]],
-                    partial(getattr(self.types, man_type),
-                            storage_data=self.storage_data))
+                return partial(getattr(self.types, man_type), storage_data=self.storage_data)
             else:
                 raise ValueError("This manager type is not specified in Types")
         elif inspect.isclass(man_type) and issubclass(man_type, BaseManager):
-            return cast(
-                Type[BaseManager[T_co, T_Storage]],
-                partial(man_type, storage_data=self.storage_data))
+            return partial(man_type, storage_data=self.storage_data)
         else:
             raise TypeError("man_type must be str or BaseManager subclass")
